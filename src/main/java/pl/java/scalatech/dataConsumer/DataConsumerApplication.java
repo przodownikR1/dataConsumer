eb5684d0275c8ce6c2b210c2ad5ea242fa72bceb
slawek <przodownikr1@gmail.com>
2018-12-24 11:21:17 +0100

    initial commit

---
+++ src/main/java/pl/java/scalatech/dataConsumer/DataConsumerApplication.java
@@ -0,0 +1,14 @@
+package pl.java.scalatech.dataConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataConsumerApplication.class, args);
+	}
+
+}
+
